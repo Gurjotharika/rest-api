2081f88ed52e44ef7ebb1cb20e836064906651fe
Gurjot Singh <79236381+Gurjotharika@users.noreply.github.com>
2021-07-14 17:37:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGO_URI: ''
+    MONGO_URI: 'mongodb://vpnlite:<RxG2lCAxetkUx9rF>@cluster0-shard-00-00.lkvea.mongodb.net:27017,cluster0-shard-00-01.lkvea.mongodb.net:27017,cluster0-shard-00-02.lkvea.mongodb.net:27017/santuyapi?ssl=true&replicaSet=atlas-10dq7e-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
