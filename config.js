54b45fadd7c148f40125831abe9ca67f9807f864
Gurjot Singh <79236381+Gurjotharika@users.noreply.github.com>
2021-07-16 08:44:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGO_URI: 'mongodb://santuy:<DxSp7JYHeMSYGpcf>@cluster0-shard-00-00.phtqh.mongodb.net:27017,cluster0-shard-00-01.phtqh.mongodb.net:27017,cluster0-shard-00-02.phtqh.mongodb.net:27017/santuyapi?ssl=true&replicaSet=atlas-flpg0i-shard-0&authSource=admin&retryWrites=true&w=majority'
+    MONGO_URI: 'mongodb://santuy:<Password>@cluster0-shard-00-00.phtqh.mongodb.net:27017,cluster0-shard-00-01.phtqh.mongodb.net:27017,cluster0-shard-00-02.phtqh.mongodb.net:27017/Database?ssl=true&replicaSet=atlas-flpg0i-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
